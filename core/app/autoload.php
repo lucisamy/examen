db4cabdb6c1395bb3b81d3fb7ac710fe97b96b2a
jmalvido <jmalvido@egibide.org>
2021-01-28 09:15:35 +0100

    Primer commit

--- core/app/autoload.php
+++ core/app/autoload.php
@@ -4,11 +4,12 @@
 // esta funcion elimina el hecho de estar agregando los modelos manualmente
 
 
-function __autoload($modelname){
+function autocargador($modelname){
 	if(Model::exists($modelname)){
 		include Model::getFullPath($modelname);
 	} 
 }
+spl_autoload_register('autocargador');
 
 
 
