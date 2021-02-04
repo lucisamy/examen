13bd260be76d087c73201d5afbad2738547aa086
lucisamy <2gdaw02@arriagainfo.local>
2021-02-04 08:32:41 +0100

    inicio

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -4,9 +4,12 @@ class Database {
 	public static $con;
 	function Database(){
 		$this->user="b8ba9d94f28aff";$this->pass="a8337514";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_f51a3950727cd14";
+		//Copiar también la información de la conexión en la función de abajo connect()
 	}
 
 	function connect(){
+		//Copiar aquí también la info de la conexión
+		$this->user="b8ba9d94f28aff";$this->pass="a8337514";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_f51a3950727cd14";
 		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
 		$con->query("set sql_mode=''");
 		return $con;
